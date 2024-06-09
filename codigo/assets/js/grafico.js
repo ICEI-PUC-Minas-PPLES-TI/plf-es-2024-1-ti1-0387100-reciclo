91274a7922f245bd8e4122be55d8d3bd930c2ad9
muriloAmachado <muriloandrademachado@gmail.com>
2024-06-09 20:43:19 -0300

    Gráfico com os tipos dos resíudos

--- codigo/assets/js/grafico.js
+++ codigo/assets/js/grafico.js
@@ -8,23 +8,31 @@ const fetchResiduos = async () => {
 // Função para contar os tipos de resíduos
 const countResiduos = (residuos) => {
     return residuos.reduce((acc, cur) => {
-        if (!acc[cur.residuesTypeId]) {
-            acc[cur.residuesTypeId] = 0;
+        if (!acc[cur.residuesTypesId]) {
+            acc[cur.residuesTypesId] = 0;
         }
-        acc[cur.residuesTypeId] += 1;
+        acc[cur.residuesTypesId] += 1;
         return acc;
     }, {});
 }
 
 // Chamada da função assíncrona e contagem dos resíduos
+const tipoResiduoLabels = {
+    1: 'Vidro',
+    2: 'Plástico',
+    3: 'Papel',
+    4: 'Metal',
+};
+
 (async () => {
     try {
         const residuos = await fetchResiduos();
         const contagemResiduos = countResiduos(residuos);
+        console.log(contagemResiduos);
 
-        const labels = Object.keys(contagemResiduos);
+        const labels = Object.keys(contagemResiduos).map(key => tipoResiduoLabels[key]);
         const data = Object.values(contagemResiduos);
-
+        
                 const ctx = document.getElementById('myPieChart').getContext('2d');
                 const myPieChart = new Chart(ctx, {
                     type: 'pie',
@@ -33,21 +41,18 @@ const countResiduos = (residuos) => {
                         datasets: [{
                             data: data,
                             backgroundColor: [
-                                'rgba(255, 99, 132, 0.2)',
+                                'rgba(174, 223, 156, 0.4)',
+                                'rgba(223, 156, 156, 0.2)',
                                 'rgba(54, 162, 235, 0.2)',
-                                'rgba(255, 206, 86, 0.2)',
-                                'rgba(75, 192, 192, 0.2)',
-                                'rgba(153, 102, 255, 0.2)',
-                                'rgba(255, 159, 64, 0.2)'
+                                'rgba(255, 206, 86, 0.2)',           
                             ],
                             borderColor: [
-                                'rgba(255, 99, 132, 1)',
+                                'rgba(174, 223, 156, 1)',
+                                'rgba(223, 156, 156, 1)',
                                 'rgba(54, 162, 235, 1)',
-                                'rgba(255, 206, 86, 1)',
-                                'rgba(75, 192, 192, 1)',
-                                'rgba(153, 102, 255, 1)',
-                                'rgba(255, 159, 64, 1)'
+                                'rgba(255, 206, 86, 1)' 
                             ],
+
                             borderWidth: 1
                         }]
                     },
@@ -55,59 +60,16 @@ const countResiduos = (residuos) => {
                         responsive: true,
                         plugins: {
                             legend: {
-                                position: 'top',
+                                position: 'bottom',
                             },
                             title: {
                                 display: true,
-                                text: 'Distribuição dos Tipos de Resíduos'
+                                text: 'Quantidade de resíduos coletados por tipo'
                             }
                         }
                     }
                 });
             } catch (error) {
                 console.error(error);
-            }
+        }
 })();
-
-// Montagem do gŕafico
-
-    function createPieChart (data) {
-    const categorias = Array.from(new Set(data.map(item => item.categoria)));
-    const valoresPorCategoria = categorias.map(categoria => {
-    const valorTotal = data.filter(item => item.categoria === categoria)
-                            .reduce((acc, curr) => acc + curr.valor, 0);
-    return valorTotal;
-        });
-
-    const ctx = document.getElementById('divPieChart');
-    const divPieChart = new Chart(ctx, {
-    type: 'pie',
-    data: {
-    labels: categorias,
-        datasets: [{
-        data: valoresPorCategoria,
-        backgroundColor: [
-            'rgba(255, 99, 132, 0.5)',
-            'rgba(54, 162, 235, 0.5)',
-            'rgba(255, 206, 86, 0.5)',
-            'rgba(75, 192, 192, 0.5)',
-            'rgba(153, 102, 255, 0.5)',
-            'rgba(255, 159, 64, 0.5)'
-        ],
-        borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-        ],
-        borderWidth: 1
-        }]
-    },
-    options: {
-        responsive: false,
-        maintainAspectRatio: false
-    }
-    });            
-}
